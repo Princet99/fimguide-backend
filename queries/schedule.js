c35de49da268c0b9ca6c98b54dca823d918293a2
Princet99 <pt1133557799@gmail.com>
2025-02-23 08:06:18 +0530

    version to 1.1 and Auth0 and improvement

--- queries/schedule.js
+++ queries/schedule.js
@@ -1,32 +1,45 @@
 const comingUpQuery = `
-  SELECT 
-      loan_no,
-      balance,
-      DATE_FORMAT(s.schedule_date, '%m/%d/%Y') AS due_date,
-      amount AS amount_due
-  FROM 
-      schedule s
-  WHERE 
-      s.loan_no = ? AND
-      s.schedule_date > ?
-      AND is_active = 1;
+   SELECT 
+    sc_ln_no AS loan_no,
+    sc_adjust AS adjustment,
+    pm_balance + sc_adjust AS balance,
+    DATE_FORMAT(sc_date, '%m/%d/%Y') AS due_date,
+    sc_due AS amount_due
+FROM 
+    schedule
+left join payment on payment.pm_sc_id = schedule.sc_id
+WHERE 
+    sc_ln_no = ?
+    AND sc_active = 'Y'
+    AND sc_payor = 2
+    limit 1;
 `;
 
 const loanStateQuery = `
 SELECT 
-    loan_no,
-    SUM(due_amount) OVER (PARTITION BY loan_no) AS total_due_amount,
-    balance,
-    schedule_date
+    sc_ln_no AS loan_no,
+    SUM(sc_due) OVER (PARTITION BY sc_ln_no) AS total_due_amount,
+    sc_balance AS balance,
+    sc_date AS schedule_date
 FROM 
     schedule
 WHERE 
-    loan_no = ? -- Replace with loan number
-    AND schedule_date <= ?
-    AND is_active = 1
+    sc_ln_no = ? -- Replace with loan number
+    AND sc_date <= ?
+    AND sc_active = 'Y'
+    AND sc_payor = 2
 ORDER BY 
-    schedule_date DESC
+    sc_date DESC
 LIMIT 1;
+
 `;
 
-module.exports = { comingUpQuery, loanStateQuery };
+const loanstateinfoQuery = `
+SELECT s.*, ss.ss_category, ss.ss_value, ss.ss_description
+FROM schedule s
+JOIN schedule_status ss
+    ON s.sc_payor = ss.ss_category_id
+WHERE s.sc_ln_no = ?  -- Replace with the actual loan number
+AND ss.ss_value = 'Lender';`;
+
+module.exports = { comingUpQuery, loanStateQuery, loanstateinfoQuery };
