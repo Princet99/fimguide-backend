5dfa5a7134cafe5fac6d1dd252c654e326b07735
Prince Thakur <60568028+Princet99@users.noreply.github.com>
2025-06-06 07:22:43 +0530

    Update app.js
    cors error

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT;
 // app.use(sessionMiddleware);
 app.use(
   cors({
-    origin: "http://localhost:3000", // Adjust this based on your frontend's URL
+    origin: "https://fimguide-frontend.vercel.app", // Adjust this based on your frontend's URL
     methods: ["GET", "POST","PUT"],
     credentials: true, // Allow credentials (cookies, session headers) to be sent
   })
